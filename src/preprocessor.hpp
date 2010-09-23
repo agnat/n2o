b45653f36159490df6bfc089f9257d7ee95502cb
David Siegel <david.siegel@artcom.de>
2010-09-23 22:08:16 +0200

    get_signature for (cv qualified) member functions

---
+++ src/preprocessor.hpp
@@ -0,0 +1,28 @@
+#ifndef N2O_PREPROCESSOR_INCLUDED
+#define N2O_PREPROCESSOR_INCLUDED
+
+# include <boost/preprocessor/cat.hpp>
+# include <boost/preprocessor/tuple/elem.hpp>
+
+# define N2O_APPLY(x) BOOST_PP_CAT(N2O_APPLY_, x)
+
+# define N2O_APPLY_N2O_ITEM(v) v
+# define N2O_APPLY_N2O_NIL
+
+
+#  define N2O_CV_COUNT 4
+
+# define N2O_CV_QUALIFIER(i)                          \
+    N2O_APPLY(                                        \
+        BOOST_PP_TUPLE_ELEM(4, i, N2O_CV_QUALIFIER_I) \
+    )
+
+# define N2O_CV_QUALIFIER_I      \
+    (                            \
+        N2O_NIL,                 \
+        N2O_ITEM(const),         \
+        N2O_ITEM(volatile),      \
+        N2O_ITEM(const volatile) \
+    )
+
+#endif //  N2O_PREPROCESSOR_INCLUDED
--- src/signature.hpp
+++ src/signature.hpp
@@ -7,6 +7,7 @@
 
 #  include <boost/preprocessor/iterate.hpp>
 
+#include "preprocessor.hpp"
 #include "type_list.hpp"
 
 #  define N2O_LIST_INC(n) \
@@ -36,7 +37,24 @@ get_signature(RT(*)(BOOST_PP_ENUM_PARAMS_Z(1, N, T)), void * = 0) {
 
 # undef N
 
+# define BOOST_PP_ITERATION_PARAMS_2 \
+    (3, (0, 3, <signature.hpp>))
+# include BOOST_PP_ITERATE()
+
 #else // BOOST_PP_ITERATION_DEPTH
+
+# define N BOOST_PP_RELATIVE_ITERATION(1)
+# define Q N2O_CV_QUALIFIER(BOOST_PP_ITERATION())
+
+template <class RT, class ClassT BOOST_PP_ENUM_TRAILING_PARAMS_Z(1, N, class T)>
+inline
+N2O_LIST_INC(BOOST_PP_INC(N))<RT, ClassT& BOOST_PP_ENUM_TRAILING_PARAMS_Z(1, N, T)>
+get_signature(RT(ClassT::*)(BOOST_PP_ENUM_PARAMS_Z(1, N, T)) Q) {
+    return N2O_LIST_INC(BOOST_PP_INC(N))<RT, ClassT& BOOST_PP_ENUM_TRAILING_PARAMS_Z(1, N, T)>();
+}
+
+# undef N
+# undef Q
 
 #endif // ! defined(BOOST_PP_IS_ITERATING)
 #endif // BOOST_PP_IS_ITERATING
