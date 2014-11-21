26761820eea74ca9f667bd975e9a7dcc34bcd037
David Siegel <david.siegel@artcom.de>
2014-11-21 15:59:24 +0100

    Added a couple of exception tests.

--- test/0030_exceptions.cpp
+++ test/0030_exceptions.cpp
@@ -1,4 +1,16 @@
 #include <n2o.hpp>
 
-N2O_MODULE(n2o::object exports) {
+#include <stdexcept>
+#include <boost/cast.hpp>
+
+using namespace n2o;
+
+void throw_bad_alloc() { throw std::bad_alloc(); }
+void throw_bad_numeric_cast() { boost::numeric_cast<unsigned>(-1); }
+void throw_out_of_range() { throw std::out_of_range("kaputt"); }
+
+N2O_MODULE(object exports) {
+    exports["throw_bad_alloc"]        = function(throw_bad_alloc);
+    exports["throw_bad_numeric_cast"] = function(throw_bad_numeric_cast);
+    exports["throw_out_of_range"]     = function(throw_out_of_range);
 }
--- test/0030_exceptions.js
+++ test/0030_exceptions.js
@@ -5,6 +5,21 @@ const test     = require('tap').test
   , root       = require('path').resolve(__dirname, '.')
   , exceptions = require('bindings')({ module_root: root, bindings: 'exceptions' });
 
+test('standard exceptions', function(t) {
+  t.plan(6);
+
+  t.type(exceptions.throw_bad_alloc, 'function');
+  t.throws(exceptions.throw_bad_alloc);
+
+  t.type(exceptions.throw_bad_numeric_cast, 'function');
+  t.throws(exceptions.throw_bad_numeric_cast);
+
+  t.type(exceptions.throw_out_of_range, 'function');
+  t.throws(exceptions.throw_out_of_range);
+
+  t.end();
+});
+
 test('exception in module init', function(t) {
   t.plan(1);
   t.throws(function() {
