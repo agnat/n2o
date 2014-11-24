cf39e634373dbedbe54def8a78bb1da0cb542b51
David Siegel <david.siegel@artcom.de>
2014-11-24 18:13:00 +0100

    Added missing v8 scope to module

--- n2o/module.hpp
+++ n2o/module.hpp
@@ -8,12 +8,14 @@
 
 #define N2O_MODULE(args) N2O_NAMED_MODULE( N2O_MODULE_NAME, args )
 
-#define N2O_NAMED_MODULE(name, args)                                                 \
-    void n2o_init_ ## name(args);                                                    \
-    void n2o_node_init_ ## name(v8::Handle<v8::Object> exports) {                    \
-        n2o::handle_exception(boost::bind(n2o_init_ ## name, n2o::object(exports))); \
-    }                                                                                \
-    NODE_MODULE(name, n2o_node_init_ ## name);                                       \
+#define N2O_NAMED_MODULE(name, args)                                          \
+    void n2o_init_ ## name(args);                                             \
+    void n2o_node_init_ ## name(v8::Handle<v8::Object> exports) {             \
+        v8::HandleScope scope(v8::Isolate::GetCurrent());                     \
+        n2o::handle_exception(                                                \
+                boost::bind(n2o_init_ ## name, n2o::object(exports)));        \
+    }                                                                         \
+    NODE_MODULE(name, n2o_node_init_ ## name);                                \
     void n2o_init_ ## name( args )
 
 #endif // N2O_MODULE_HPP_INCLUDED_
