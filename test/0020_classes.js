fd63ebd330028ad8d02848bff46e68e24e243d4f
David Siegel <david.siegel@artcom.de>
2014-11-24 22:10:42 +0100

    Wrote some more class tests.

--- test/0020_classes.js
+++ test/0020_classes.js
@@ -6,12 +6,18 @@ const test  = require('tap').test
   , classes = require('bindings')({ module_root: root, bindings: 'classes' });
 
 test("class hello world", function(t) {
-  t.plan(4);
+  t.plan(9);
 
   t.type(classes.World, 'function');
   t.strictEquals(classes.World.name, 'World');
   t.type(new classes.World(), 'object');
-  t.throws(function() { classes.World() }, "constructor called without 'new' must throw");
+  t.throws(classes.World, 'constructor called without "new" must throw');
+
+  t.ok((new classes.World()) instanceof classes.World);
+  t.strictEquals((new classes.World()).__proto__, classes.World.prototype);
+  t.ok('greet' in classes.World.prototype);
+  t.type(classes.World.prototype['greet'], 'function');
+  t.ok('greet' in new classes.World());
 
   t.end();
 });
