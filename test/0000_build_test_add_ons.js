216c1c5dffa8f8e8e0b9f06bb8be3dea18a8ac02
David Siegel <david.siegel@artcom.de>
2014-11-24 18:09:29 +0100

    Rebuild test add-ons more gently.

--- test/0000_build_test_add_ons.js
+++ test/0000_build_test_add_ons.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
-const test = require('tap').test
+const test  = require('tap').test
     , spawn = require('child_process').spawn
+    , path  = require('path')
+    , fs    = require('fs')
     ;
 
 test('build test add-ons', function(t) {
   t.plan(1);
-  spawn('node-gyp', ['rebuild'], { 'cwd': __dirname, 'stdio': 'inherit'})
-    . on('close', function(code) { t.strictEqual(code, 0); t.end();})
-    ;
+
+  fs.exists(path.resolve(__dirname, 'build', 'Makefile'), function(exists) {
+    var command = exists ? 'build' : 'rebuild';
+    console.log(command);
+    spawn('node-gyp', [command], { 'cwd': __dirname, 'stdio': 'inherit'})
+      . on('close', function(code) { t.strictEqual(code, 0); t.end();})
+      ;
+  });
 });
